5cb48f923748c17a1a4438f4704aa7053120f794
phenibut645 <doe481157@gmail.com>
2025-08-21 16:06:05 +0300

    PUL-2: more valid addresses for IPv4 scheme test

--- services/svc-pinger/test/ip.scheme.test.ts
+++ services/svc-pinger/test/ip.scheme.test.ts
@@ -3,7 +3,7 @@ import { ZodError } from 'zod'
 import { ipv4Scheme } from '../src/schemes/ip.scheme'
 
 const invalidAddresses = ["123", "002.001", "999.999.999.999", "qwerty.com", "266.266.266.266"]
-const validAddresses = ["172.16.254.1", "192.168.0.1", "11.22.33.44"]
+const validAddresses = ["172.16.254.1:8000", "192.168.0.1", "11.22.33.44", "172.16.254.1:8000/code?as=123"]
 
 describe("IPv4 scheme", () => {
     it.each(invalidAddresses)("Should throw the validation error", async (address) => {
