e1fa6b7aad6a5917a5d20de1263b5ab89ccd3f4e
phenibut645 <doe481157@gmail.com>
2025-09-04 22:47:54 +0300

    PUL-9: migration changes

--- services/pinger/src/db/migration-runner.ts
+++ services/pinger/src/db/migration-runner.ts
@@ -1,10 +1,13 @@
+import makeConnection from './conn';
 import { CreatePingSetupsIndex } from './migrations/001-create-pingSetup-index';
 
 const migrationFuncs = [CreatePingSetupsIndex];
 
 async function run() {
+  const db = await makeConnection();
+
   migrationFuncs.forEach((func) => {
-    func();
+    func(db);
   });
 }
 
--- services/pinger/src/db/migrations/001-create-pingSetup-index.ts
+++ services/pinger/src/db/migrations/001-create-pingSetup-index.ts
@@ -1,13 +1,11 @@
-import makeConnection, { COLLECTION, closeDbConnection } from '../conn';
+import { Db } from 'mongodb';
+import { COLLECTION } from '../conn';
 
-export async function CreatePingSetupsIndex() {
-  const db = await makeConnection();
+export async function CreatePingSetupsIndex(db: Db) {
   const collection = db.collection(COLLECTION.pingSetups);
 
   await collection.createIndex(
     { userUuid: 1, target: 1 },
     { unique: true, name: 'userUuid_target_unique' },
   );
-
-  closeDbConnection();
 }
